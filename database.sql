32a1cc931b1209fac75a3bb07edae6b7f1617bf7
amr33mohamd <amr2010mohamd2010@gmail.com>
2017-12-01 18:21:25 +0200

    database update

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2017 at 01:10 PM
+-- Generation Time: Dec 01, 2017 at 05:20 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -53,8 +53,36 @@ CREATE TABLE `books` (
   `sub_category_id` int(11) NOT NULL,
   `link` text NOT NULL,
   `downloads` int(11) NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `image` text NOT NULL,
+  `descc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `name`, `category_id`, `sub_category_id`, `link`, `downloads`, `price`, `image`, `descc`) VALUES
+(3, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(4, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(5, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(6, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(7, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(8, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(9, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(10, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(11, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(12, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(13, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(14, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(15, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(16, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(17, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(18, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(19, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(20, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(21, 'المتنبى ف الفصحي', 1, 2, 'http://google.com', 5, 10, '', ''),
+(22, 'aaa', 0, 0, 'books/0.6858965634742721.jpg', 0, 0, 'images/0.685896563474272.jpg', 'aaaa');
 
 -- --------------------------------------------------------
 
@@ -80,6 +108,14 @@ CREATE TABLE `categories` (
   `name` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'كتب'),
+(2, 'مجلات');
+
 -- --------------------------------------------------------
 
 --
@@ -97,6 +133,18 @@ CREATE TABLE `reviews` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `screenshots`
+--
+
+CREATE TABLE `screenshots` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sub_categories`
 --
 
@@ -105,6 +153,14 @@ CREATE TABLE `sub_categories` (
   `name` text NOT NULL,
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `sub_categories`
+--
+
+INSERT INTO `sub_categories` (`id`, `name`, `category_id`) VALUES
+(0, 'الفكر', 1),
+(0, 'الوجدان', 2);
 
 -- --------------------------------------------------------
 
@@ -124,8 +180,33 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `country`, `address`, `currancy`, `rule`) VALUES
+(1, 'admin', 'admin', 'admin', '', '', '', 1);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `screenshots`
+--
+ALTER TABLE `screenshots`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -138,10 +219,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `screenshots`
+--
+ALTER TABLE `screenshots`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
