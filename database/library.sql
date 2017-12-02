b3ffd141372fb895211bedbc69f8516fc25308e8
Khalid <khalidahmed333@hotmail.com>
2017-12-02 06:42:21 +0200

    Update "orders" table in database
    Add key, and AUTO INCREMENT.

--- database/library.sql
+++ database/library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2017 at 03:34 AM
+-- Generation Time: Dec 02, 2017 at 05:39 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -118,7 +118,14 @@ CREATE TABLE `orders` (
   `book_id` int(11) NOT NULL,
   `method` int(11) NOT NULL,
   `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `book_id`, `method`, `status`) VALUES
+(1, 1, 3, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -188,7 +195,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `country`, `address`, `currency`, `rule`) VALUES
-(1, 'admin', 'admin', 'admin', '', '', 0, 1);
+(1, 'admin', 'admin', 'admin', 'Egypt', '8 Mile', 0, 1);
 
 --
 -- Indexes for dumped tables
@@ -204,6 +211,12 @@ ALTER TABLE `books`
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -238,6 +251,11 @@ ALTER TABLE `books`
 --
 ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `screenshots`
 --
