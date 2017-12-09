a1c9c200b50d905bc498cae3e274048a1d9ab734
Khalid <khalidahmed333@hotmail.com>
2017-12-10 00:08:36 +0200

    Add missing requires

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ require('./routes/buy/buy-first.js');
 require('./routes/buy/buy-second');
 require('./routes/api/feedback');
 require('./routes/api/set_currency');
+require('./routes/api/signin');
+require('./routes/api/signup');
+require('./routes/api/orders');
+require('./routes/api/search');
+require('./routes/feedbacks');
