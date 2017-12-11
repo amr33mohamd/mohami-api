7ebb0afd47874830a919294bcd3704c09ea95180
amr33mohamd <amr2010mohamd2010@gmail.com>
2017-12-12 00:58:07 +0200

    fake price added to admin panel

--- routes/api/books_of_cat.js
+++ routes/api/books_of_cat.js
@@ -1,6 +1,6 @@
 app.get('/api/books_of_cat',function(req,res){
     var cat_id = req.param("cat_id");
-    con.query('SELECT id, name AS book_name, image AS book_photo, author_name FROM books WHERE category_id=?', [cat_id], function(err,data) {
+    con.query('SELECT id, name AS book_name, image AS book_photo, author_name, fake_price AS discount FROM books WHERE category_id=?', [cat_id], function(err,data) {
         if(!err) {
             res.send(data);
         }
