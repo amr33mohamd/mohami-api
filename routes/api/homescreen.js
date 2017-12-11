30f5139c46f5b9cfcee36342441b12f6b90835dd
amr33mohamd <amr2010mohamd2010@gmail.com>
2017-12-11 22:35:58 +0200

    api show fake price

--- routes/api/homescreen.js
+++ routes/api/homescreen.js
@@ -3,7 +3,7 @@ app.get('/api/homescreen',function(req,res){
   data = [];
   sql.select('categories','1','1',function(categories){
     for(let counter in categories){
-      con.query("SELECT id AS book_ID, name AS book_name, image AS book_photo, author_name FROM books WHERE category_id=? LIMIT 3",
+      con.query("SELECT id AS book_ID, name AS book_name, image AS book_photo, author_name,fake_price FROM books WHERE category_id=? LIMIT 3",
       [categories[counter]['id']],function(err,books){
           data.push({
             cat_ID: categories[counter]['id'],
