7c54f19f7960177b2b24d6c6afc0644fc92775ee
Khalid <khalidahmed333@hotmail.com>
2017-12-12 01:45:53 +0200

    Price & discount algorithm in api/price_of_book

--- routes/api/books_of_subcat.js
+++ routes/api/books_of_subcat.js
@@ -1,6 +1,6 @@
 app.get('/api/books_of_subcat',function(req,res){
     var cat_id = req.param("sub_cat_id");
-    con.query('SELECT id, name AS book_name, image AS book_photo, author_name ,fake_price AS discount FROM books WHERE sub_category_id=?', [cat_id], function(err,data) {
+    con.query('SELECT id, name AS book_name, image AS book_photo, author_name FROM books WHERE sub_category_id=?', [cat_id], function(err,data) {
         if(!err) {
             res.send(data);
         }
--- routes/api/price_of_book.js
+++ routes/api/price_of_book.js
@@ -1,37 +1,80 @@
-
 app.get('/api/price_of_book',function(req,res){
-
-
     var book_id = req.param("book_id");
     var convert = req.param("convert");
-    con.query('SELECT price FROM books WHERE id=? LIMIT 1', [book_id], function(err,data) {
+    con.query('SELECT price, fake_price AS discount FROM books WHERE id=? LIMIT 1', [book_id], function(err,data) {
         if(!err) {
-            if(data[0]['price'] == 0)
-            {
-                res.json({
-                    price: 0
-                });
-            }
-            else if(convert == 1)
+            var price = data[0]['price'], discount = data[0]['discount'];
+            if(convert == 1)
             {
                 const converter = require('google-currency');
-                converter({
-                    from: "SAR",
-                    to: "USD",
-                    amount: data[0]['price']
-                }).then(value => {
-                    res.json({
-                        price: parseFloat(value.converted.toFixed(2))
-                    });
-                })
+                if(price > 0)
+                {
+                    converter({
+                        from: "SAR",
+                        to: "USD",
+                        amount: price
+                    }).then(value => {
+                        price = parseFloat(value.converted.toFixed(2));
+
+                        if(discount > 0)
+                        {
+                            converter({
+                                from: "SAR",
+                                to: "USD",
+                                amount: discount
+                            }).then(value2 => {
+                                discount = parseFloat(value2.converted.toFixed(2));
+
+                                res.json({
+                                    price: price,
+                                    discount: discount
+                                });
+
+                            })
+                        }
+                        else
+                        {
+                            res.json({
+                                price: price,
+                                discount: 0
+                            });
+                        }
+                    })
+                }
+                else
+                {
+                    if(discount > 0)
+                    {
+                        converter({
+                            from: "SAR",
+                            to: "USD",
+                            amount: discount
+                        }).then(value => {
+                            discount = parseFloat(value.converted.toFixed(2));
+
+                            res.json({
+                                price: 0,
+                                discount: discount
+                            });
+
+                        })
+                    }
+                    else
+                    {
+                        res.json({
+                            price: 0,
+                            discount: 0
+                        });
+                    }
+                }
             }
             else
             {
                 res.json({
-                    price: data[0]['price']
+                    price: price,
+                    discount: discount
                 });
             }
-
         }
         else
         {
