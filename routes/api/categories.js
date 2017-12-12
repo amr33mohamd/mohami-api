b25fea4df7a60b4220546b53ce853443ba129689
Khalid <khalidahmed333@hotmail.com>
2017-12-12 02:30:39 +0200

    Show oldest categories first in home screen

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,5 +1,5 @@
 app.get('/api/categories',function(req,res){
-    con.query('SELECT id, name FROM categories', function(err,data) {
+    con.query('SELECT id, name FROM categories ORDER BY id ASC', function(err,data) {
         if(!err) {
             res.send(data);
         }
