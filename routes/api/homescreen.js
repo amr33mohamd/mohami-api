af3113f321a52416837ba98ffc1999ec21d87c6e
Khalid <khalidahmed333@hotmail.com>
2017-12-12 03:09:10 +0200

    Addendum to last commit

--- routes/api/homescreen.js
+++ routes/api/homescreen.js
@@ -1,22 +1,23 @@
 app.get('/api/homescreen',function(req,res){
 
   data = [];
-  sql.select('categories','1','1',function(categories){
-    for(let counter in categories){
-      con.query("SELECT id AS book_ID, name AS book_name, image AS book_photo, author_name FROM books WHERE category_id=? ORDER BY id DESC LIMIT 3",
-      [categories[counter]['id']],function(err,books){
-          data.push({
-            cat_ID: categories[counter]['id'],
-            cat_name : categories[counter]['name'],
-            cat_books: books,
-          });
-          if(counter == categories.length-1){
-            res.json(data);
+  con.query('SELECT id, name FROM categories ORDER BY id ASC', function(err,data) {
+      if(!err) {
+          for(let counter in categories){
+            con.query("SELECT id AS book_ID, name AS book_name, image AS book_photo, author_name FROM books WHERE category_id=? ORDER BY id DESC LIMIT 3",
+            [categories[counter]['id']],function(err,books){
+                data.push({
+                  cat_ID: categories[counter]['id'],
+                  cat_name : categories[counter]['name'],
+                  cat_books: books,
+                });
+                if(counter == categories.length-1){
+                  res.json(data);
+                }
+
+            })
           }
-
-      })
-    }
+      }
   })
-
 });
 // var group_id = req.param('group_id');
