ea356752cc245774bd2e3b71043e934759ef7783
amr33mohamd <amr2010mohamd2010@gmail.com>
2017-12-28 03:40:18 +0200

    aa

--- routes/buy/buy-second.js
+++ routes/buy/buy-second.js
@@ -2,7 +2,7 @@ app.get('/buy-second',function(req,res){
   var book_id = req.param('book_id');
   var user_id = req.param('user_id');
   var method = req.param('method');
-  
+
   sql.select('books','id',book_id,function(book) {
       con.query('insert into orders(user_id,book_id,method,status) values(?,?,?,0)',[user_id,book_id,method],function(err,ress){
         if(err){
