28b63d6e438319138b701b5b3158734357733221
Khalid <khalidahmed333@hotmail.com>
2018-03-10 01:10:01 +0200

    Return books of the same category if no similar books were found

--- routes/api/similar_books.js
+++ routes/api/similar_books.js
@@ -1,14 +1,14 @@
 app.get('/api/similar_books',function(req,res){
     var book_id = req.param("book_id");
-    con.query('SELECT name, author_name FROM books WHERE id=? LIMIT 1', [book_id], function(err,bookData) {
+    con.query('SELECT name, author_name, category_id FROM books WHERE id=? LIMIT 1', [book_id], function(err,bookData) {
         if(!err) {
             var correctQueries = [
                 "%" + bookData[0].name + "%",
                 "%" + bookData[0].author_name + "%"
             ];
             con.query(`SELECT id, name AS book_name, image AS book_photo, author_name,
-                category_id FROM books WHERE (name LIKE ?) OR (author_name LIKE ?) OR (descc LIKE ?) LIMIT 5`,
-                [correctQueries[0], correctQueries[1], correctQueries[0]],
+                category_id FROM books WHERE ((name LIKE ?) OR (author_name LIKE ?) OR (descc LIKE ?) AND id != ?) LIMIT 5`,
+                [correctQueries[0], correctQueries[1], correctQueries[0], book_id],
             function(err,search_res) {
                 if(!err) {
                     if(search_res.length > 0)
@@ -54,7 +54,28 @@ app.get('/api/similar_books',function(req,res){
                     }
                     else
                     {
-                        res.json({status:0});
+                        con.query(`SELECT id, name AS book_name, image AS book_photo, author_name FROM books WHERE category_id=? AND id != ? LIMIT 5`,
+                            [bookData[0].category_id, book_id],
+                        function(err,search_res) {
+                            if(!err) {
+                                con.query('select name FROM categories WHERE id=? LIMIT 1', [ bookData[0].category_id ], function(err,cat) {
+                                    if(!err)
+                                    {
+                                        data = [];
+
+                                        search_res.forEach(function(asearch_res) {
+                                            data.push({ ...asearch_res, cat_name: cat[0].name });
+                                        });
+
+                                        res.json({
+                                          status:1,
+                                          result:data
+                                        })
+                                    }
+                                    else res.json({status:0});
+                            }
+                            else res.json({status:0});
+                        });
                     }
                 }
                 else
