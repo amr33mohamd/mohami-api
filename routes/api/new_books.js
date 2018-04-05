667eb04c72da05351d9c4d735a833635fa02aad8
Khalid <khalidahmed333@hotmail.com>
2018-04-05 22:31:04 +0200

    Return recent 50 books instead of 20

--- routes/api/new_books.js
+++ routes/api/new_books.js
@@ -1,5 +1,5 @@
 app.get('/api/new_books',function(req,res){
-    con.query('SELECT id, name AS book_name, image AS book_photo, author_name FROM books ORDER BY id DESC LIMIT 20', function(err,data) {
+    con.query('SELECT id, name AS book_name, image AS book_photo, author_name FROM books ORDER BY id DESC LIMIT 50', function(err,data) {
         if(!err) {
             res.send(data);
         }
