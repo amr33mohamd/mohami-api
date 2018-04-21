6024cb45d02afe65dad61dc53a40a17e77145617
Khalid <khalidahmed333@hotmail.com>
2018-04-21 04:03:12 +0200

    Fix a bug: feedback sender didn't exist in database

--- routes/api/feedback.js
+++ routes/api/feedback.js
@@ -3,13 +3,13 @@ app.get('/api/send_feedback',function(req,res){
   var message = req.param('message');
 
   con.query('select name, email from users where id=? LIMIT 1',[user_id],function(err,user){
-    if(err){
-      res.json({status:0});
+    if(err || !user.length){
+      res.json({status:0, err});
     }
     else {
         con.query('insert into feedbacks(name,email,message) values(?,?,?)',[ user[0]['name'], user[0]['email'], message],function(err,ress){
           if(err){
-            res.json({status:0});
+            res.json({status:0, err});
           }
           else {
             res.json({status:1});
