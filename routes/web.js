407867ed54042e88439c92d4baf95acce95ac9a6
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-07-27 21:50:41 +0200

    admin panel

--- routes/web.js
+++ routes/web.js
@@ -169,6 +169,85 @@ app.get('/add-categories', function(req, res) {
 		});
 	});
 });
+app.get('/api/add-token',function(req,res){
+	var token = req.param('token');
+	sql.select('tokens','token',token,function(data){
+		if(data.length == 0 ){
+			con.query('insert into tokens(token) values(?)',[token],function(err,data){
+				if(!err){
+					res.json({response:1});
+				}
+				else {
+					res.json({response:0});
+				}
+			})
+		}
+		else {
+			res.json({response:0});
+		}
+
+	})
+})
+app.get('/notify',function(req,res){
+	res.render('notify');
+})
+app.get('/send_notify',function(req,res){
+var	messege = req.param('messege');
+	sql.select('tokens','1','1',function(data){
+		if(SendPushNotifications(data,message)){
+			res.redirect('/notify')
+		}
+
+	})
+})
+function SendPushNotifications(pushTokens,message_body)
+{
+    const Expo = require('expo-server-sdk');
+
+    // Create a new Expo SDK client
+    let expo = new Expo();
+
+    // Create the messages that you want to send to clents
+    let messages = [];
+    for (let i in pushTokens) {
+        // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+        // Check that all your push tokens appear to be valid Expo push tokens
+        if (!Expo.isExpoPushToken(pushTokens[i].token)) {
+            console.error(`Push token ${pushToken} is not a valid Expo push token`);
+            continue;
+        }
+
+        // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+        messages.push({
+            to: pushTokens[i].token,
+            sound: 'default',
+            body: message_body,
+            data: { },
+        })
+    }
+
+    // The Expo push notification service accepts batches of notifications so
+    // that you don't need to send 1000 requests to send 1000 notifications. We
+    // recommend you batch your notifications to reduce the number of requests
+    // and to compress them (notifications with similar content will get
+    // compressed).
+    let chunks = expo.chunkPushNotifications(messages);
+
+    (async () => {
+        // Send the chunks to the Expo push notification service. There are
+        // different strategies you could use. A simple one is to send one chunk at a
+        // time, which nicely spreads the load out over time:
+        for (let chunk of chunks) {
+            try {
+                let receipts = await expo.sendPushNotificationsAsync(chunk);
+                console.log(receipts);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })();
+}
 
 app.post('/add_category', function(req, res) {
 	var name = req.body.name;
