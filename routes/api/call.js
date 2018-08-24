b7b213da9adde79c3456d17974d4665d1f218fa0
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-08-24 17:31:06 +0200

    add lawyer

--- routes/api/call.js
+++ routes/api/call.js
@@ -4,8 +4,8 @@ app.get('/api/call',function(req,res){
   var phone = req.param('phone');
   var email = req.param('email');
   var message = req.param('message');
-
-    con.query('insert into calls(name,phone,email,message) values(?,?,?,?) ',[name,phone,email,message], function(err,data) {
+  var user_name = req.param('user_name')
+    con.query('insert into calls(name,phone,email,message,user_id,user_name) values(?,?,?,?,?,?) ',[name,phone,email,message,user_id,user_name], function(err,data) {
       if(!err){
         res.json({response:true});
       }
